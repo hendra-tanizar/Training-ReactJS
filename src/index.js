d80f05c1316c14bbd6ec2bdd5b5331e0cfb885a3
hendra-tanizar <hendra@student.ukdc.ac.id>
2023-08-08 09:56:25 +0700

    latihan-reactjs

--- src/index.js
+++ src/index.js
@@ -4,12 +4,32 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// function formatName(user){
+//   return user.firstName + ' ' + user.lastName;
+// }
+// const user = {
+//   firstName: 'Hendra',
+//   lastName: 'Nicholas'
+// };
+// const element = (
+//   <h1> 
+//   Berca
+//   </h1>
+// );
+
+function Welcome(props){
+  return <h1> Hello, {props.name} </h1>
+}
+function Name(){
+  return (
+    <div>
+      <Welcome name="Hendra"/>
+      <Welcome name="Nicholas"/>
+    </div>
+  );
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<Name/>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
